d620b44e53b79b36de0db87ce84d3185edba7c0c
root <xiaotian.wu>
2015-01-04 21:30:00 +0800

    modify executor

--- executor.py
+++ executor.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+
 __author__ = 'xiaotian.wu@chinacache.com'
 
+import os
 import sys
 import threading
 
@@ -8,20 +11,28 @@ import mesos.interface
 from mesos.interface import mesos_pb2
 
 class DockerExecutor(mesos.interface.Executor):
-    def launchTask(self, driver, task):
-        print("running task %s" % task.task_id.value)
-        update = mesos_pb2.TaskStatus()
-        update.task_id.value = task.task_id.value
-        update.state = mesos_pb2.TASK_RUNNING
-        driver.sendStatusUpdate(update)
-        print("running status...")
-        update.state = mesos_pb2.TASK_FINISHED
-        driver.sendStatusUpdate(update)
-        #running_thread = threading.Thread(target = run)
-        #running_thread.start()
+  def launchTask(self, driver, task):
+    def run():
+      command = task.data
+      ret = os.popen(command)
+      update = mesos_pb2.TaskStatus()
+      update.task_id.value = task.task_id.value
+      update.state = mesos_pb2.TASK_FINISHED
+      update.message = ret.read()
+      print("task finished, return message: %s" % update.message)
+      driver.sendStatusUpdate(update)
 
-    def frameworkMessage(self, driver, message):
-        driver.sendFrameworkMessage(message)
+    print("running task %s, command: %s" % (task.task_id.value, task.data))
+    update = mesos_pb2.TaskStatus()
+    update.task_id.value = task.task_id.value
+    update.state = mesos_pb2.TASK_RUNNING
+    driver.sendStatusUpdate(update)
+    print("task is running...")
+    running_thread = threading.Thread(target = run)
+    running_thread.start()
+
+  def frameworkMessage(self, driver, message):
+    driver.sendFrameworkMessage(message)
 
 if __name__ == "__main__":
     driver = mesos.native.MesosExecutorDriver(DockerExecutor())
