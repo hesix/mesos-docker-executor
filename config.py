f65dba8559046d646ced3c4dd70d6b90b949f4c5
hesix <qiang.he@chinacache.com>
2015-02-12 10:05:35 +0000

    fix the PYTHONPATH

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ __author__ = "qiang.he@chinacache.com"
 import logging
 import logging.config
 
-logging.config.fileConfig("logging.conf")
+logging.config.fileConfig("/mesos-docker-executor/logging.conf")
 logger = logging.getLogger("log")
